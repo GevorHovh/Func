e103f1860925b86be29f3e31e823fd4b86558603
GevorHovh <123312885+GevorHovh@users.noreply.github.com>
2023-02-02 13:25:55 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,43 @@
+/*
+function calc(a) {
+    let b = 10;
+    console.log(a + b )
+}
+
+calc(5);
+*/
+/*
+ // gtnel bolor tveri gumary
+let array = [5,10,5,8,7,9,6];
+function calc(sum){
+for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < 1; j++) {
+        sum += array[i];
+        
+    }
+}
+console.log(sum);
+
+};
+calc(0)
+*/
+
+/* // 2 tveric vorne aveli mec
+function bigger(num1, num2) {
+    if(num1 > num2) return num1
+    return num2
+}
+
+console.log(bigger(50, 20));
+console.log(bigger(5, 20));
+*/
+/* // 3 tveric vorn e aveli mec
+function bigger(num1, num2, num3) {
+    if(num1 > (num2 + num3) / 2)return num1
+    else if (num2 > (num1 + num3) / 2)return num2;
+    else if(num3 > (num1 + num2) / 2)return num3
+    
+}
+
+console.log(bigger(12,25,20));
+*/
